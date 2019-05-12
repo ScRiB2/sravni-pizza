d302eb95c84f4dd1ae7f4aec6bc17018aaa5a2c3
ScRiB <scrib2@yandex.ru>
2019-05-12 18:34:59 +0300

    Теперь пользоватеил связаны с PostgreSQL

--- src/main/java/ru/scrib/spring/config/AppConfig.java
+++ src/main/java/ru/scrib/spring/config/AppConfig.java
@@ -1,18 +1,29 @@
 package ru.scrib.spring.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
 @Configuration
 @EnableWebMvc
-@ComponentScan("ru.scrib.spring")
+@ComponentScan(basePackages = "ru.scrib.spring")
+@PropertySource("classpath:persistence-postgresql.properties")
 public class AppConfig implements WebMvcConfigurer {
+
+    @Autowired
+    Environment env;    //For hold data read from properties files
 
     @Bean
     public ViewResolver viewResolver() {
@@ -26,5 +37,23 @@ public class AppConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public DataSource securityDataSource(){
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+        securityDataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
+        securityDataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
+        securityDataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
+        securityDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
+        return securityDataSource;
     }
 }
--- src/main/java/ru/scrib/spring/config/SecurityConfig.java
+++ src/main/java/ru/scrib/spring/config/SecurityConfig.java
@@ -1,24 +1,25 @@
 package ru.scrib.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("john").password("1234").roles("EMPLOYEE"))
-                .withUser(users.username("mary").password("1234").roles("MANAGER"))
-                .withUser(users.username("susan").password("1234").roles("ADMIN", "EMPLOYEE"));
+        auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override
