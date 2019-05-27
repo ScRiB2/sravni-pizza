702266d31284424e429e607e087f3d55c8c18d17
ScRiB <scrib2@yandex.ru>
2019-05-27 15:36:55 +0300

    Убрал часть лишнего кода

--- src/main/java/ru/scrib/spring/controller/CatalogController.java
+++ src/main/java/ru/scrib/spring/controller/CatalogController.java
@@ -4,16 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.scrib.spring.entity.pizza.Company;
-import ru.scrib.spring.entity.pizza.Pizza;
-import ru.scrib.spring.filters.CompanyFilter;
 import ru.scrib.spring.filters.Filters;
 import ru.scrib.spring.service.CompanyService;
 import ru.scrib.spring.service.PizzaService;
-import ru.scrib.spring.string.StringHelper;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Controller
@@ -28,10 +21,6 @@ public class CatalogController {
     @GetMapping("/list")
     public String list(Model model) {
         Filters filters = new Filters();
-        List<CompanyFilter> companyFilters= new ArrayList<>();
-        List<Company> companies = companyService.getCompanies();
-
-        filters.setCompanies(new String[]{"Yummy"});
         filters.setMinPrice(0);
         filters.setMaxPrice(1000);
         filters.setSort(0);
